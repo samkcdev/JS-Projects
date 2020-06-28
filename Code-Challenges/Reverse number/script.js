65b265d031549e7cd53498457c954ee21e53676c
Sam Cherian <samnp87@gmail.com>
2020-06-28 22:11:20 +0530

    Removed consoles logs and printing result in browser

--- Code-Challenges/Reverse number/script.js
+++ Code-Challenges/Reverse number/script.js
@@ -1,24 +1,18 @@
 let inputForm = document.querySelector("input");
 let btn = document.querySelector("button");
-console.log(inputForm.value.length);
 
 btn.addEventListener("click", reverseNum);
-
-if (inputForm.value.length > 5) {
-}
 
 function reverseNum() {
   if (inputForm.value.length === 5) {
     let inputVal = inputForm.value.split("");
-    console.log(inputVal.length);
     let reverseNum = "";
     for (let i = inputVal.length - 1; i >= 0; i--) {
-      console.log("i", i);
       let lastVal = inputVal.pop();
-      console.log("inputVal", inputVal);
       reverseNum += lastVal;
     }
-    console.log("reverseNum", reverseNum);
+    let resultspan = document.querySelector("div");
+    resultspan.innerHTML = "<p>" + `Result is ${Number(reverseNum)}` + "</p>";
   } else if (inputForm.value.length > 5) {
     btn.disabled = true;
   }
