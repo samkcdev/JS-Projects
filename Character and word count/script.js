06ecf14da46aed1d63f6a8640b8a70255d5117fb
Sam Cherian <samnp87@gmail.com>
2020-07-30 08:38:50 +0530

    Word count task completed

--- Character and word count/script.js
+++ Character and word count/script.js
@@ -1,8 +1,29 @@
 let writingArea = document.querySelector("textarea");
+let counter = document.getElementById("count-bar");
+// counter.innerHTML = `Number of Characters are ${writingArea.value.length}`;
+writingArea.focus();
+console.log(document.activeElement.tagName);
+let wordCount = [];
+writingArea.addEventListener(
+  "input",
+  (event) => {
+    console.log(event);
 
-writingArea.addEventListener("keypress", (event) => {
-  console.log(event);
-  let counter = document.getElementById("count-bar");
-  counter.innerHTML = `Number of Characters are ${writingArea.value.length}`;
-  //  if(event.keyCode!==){}
-});
+    let wordcountSplit = writingArea.value
+      //split based on regex that takes space \n\r\s
+      .split(/[\n\r\s]+/g)
+      //we get an array of chunks of what we type
+      //we filter out only if array have value a new array is returned
+      .filter((word) => {
+        return word.length > 0;
+      });
+
+    console.log(wordcountSplit);
+
+    // if (event.keyCode == 32) {
+    //   wordCount++;
+    // }
+    counter.innerHTML = `Count is ${wordcountSplit.length} Number of Characters are ${writingArea.value.length} `;
+  },
+  false
+);
